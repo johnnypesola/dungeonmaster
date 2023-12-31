fbc59ab41929c1ac975736dadf8a7d8476a63dac
Johnny Pesola <johnny@pesola.eu>
2023-12-31 10:37:39 +0100

    wip room logic

--- frontend/client.js
+++ frontend/client.js
@@ -1,14 +1,41 @@
+const getRoom = async (roomNum) => {
+  const response = await fetch("http://localhost:3000/api/room/" + roomNum);
+  return await response.json();
+};
 
+const setDescription = (description) => {
+  const descritionEl = document.querySelector(".room-info");
+  descritionEl.innerHTML = description;
+};
 
-var request = new XMLHttpRequest()
-request.open('Get','http://localhost:3000/api/room/1', true)
-request.onload = function (){
+const updateHtmlWithRoomData = (room) => {
+  setDescription(room.description);
 
-}
-request.send()
+  if (room.N) {
+    console.log("got N");
+    // enable N button and attach right eventhandler to it.
+    const arrowN = document.querySelector(".arrow-N");
+    arrowN.addEventListener("click", async (e) => {
+      const room = await getRoom(firstRoom.N.to_room);
+    });
+  }
+  if (room.E) {
+    console.log("got E");
+  }
+  if (room.S) {
+    console.log("got S");
+  }
+  if (room.W) {
+    console.log("got W");
+  }
+};
 
-const arrowN = document.querySelector(".arrow-N");
-arrowN.addEventListener("click",Function('Get','http://localhost:3000/api/room/1'){
-    
-})
-console.log("hej")
+const initWithFirstRoom = () => {
+  const firstRoomPromise = getRoom(0);
+  firstRoomPromise.then((firstRoom) => {
+    console.log(firstRoom);
+    updateHtmlWithRoomData(firstRoom);
+  });
+};
+
+initWithFirstRoom()
